5df61c648ef5b80e98f06d7ae2ad1f70f85a0949
GuilhermeAugustoP <guilherme.augustovp@gmail.com>
2022-09-30 13:25:05 -0300

    Image component
    Working on image component.

---
+++ src/ImagesComponents.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Irwin from './img/Irwin-Ishad.png'
+import './Images.css'
+
+function ImgAspRatio () {
+    return (
+        <div className='container'>
+            <img alt='Foto do Aniversariante' src={Irwin} className='mainImg'></img>
+        </div>
+    )
+}
+
+
+export default ImgAspRatio
