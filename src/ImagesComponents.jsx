2cafbaea82696f683de644bd5d378c8a15ecb5cf
TyAssis <tyassis3@gmail.com>
2022-10-04 20:23:20 -0300

    add img component css properties

--- src/ImagesComponents.jsx
+++ src/ImagesComponents.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import Irwin from './img/Irwin-Ishad.png'
+import Irwin from './img/trilha.jpg'
 import './Images.css'
 
 function ImgAspRatio () {
     return (
         <div className='container'>
-            <img alt='Foto do Aniversariante' src={Irwin} className='mainImg'></img>
+            <img 
+                alt='Foto do Aniversariante' 
+                src={Irwin} 
+                className='mainImg' 
+            />
         </div>
     )
 }
